c05f554159ce3800277d43f7d27c6b1e60f53dc6
Barbara2004 <Barbaraabr04@gmail.com>
2022-10-23 01:20:23 +0300

    ADDED OrderController & ProductController

--- src/main/java/com/hillel/store/controller/order/OrderControllerBean.java
+++ src/main/java/com/hillel/store/controller/order/OrderControllerBean.java
@@ -1,32 +1,48 @@
 package com.hillel.store.controller.order;
 
+import com.hillel.store.domain.Address;
 import com.hillel.store.domain.Order;
+import com.hillel.store.service.order.OrderService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
+@AllArgsConstructor
+@RequestMapping(value = "/store_api", produces = MediaType.APPLICATION_JSON_VALUE)
 public class OrderControllerBean implements OrderController {
+
+    private final OrderService orderService;
+
     @Override
-    public Order create(Order object) {
-        return null;
+    @PostMapping("/order")
+    public Order create(@RequestBody Order order) {
+        return orderService.create(order);
     }
 
     @Override
+    @GetMapping("/order")
     public List<Order> getAll() {
-        return null;
+        return orderService.getAll();
     }
 
     @Override
-    public Order getById(Integer id) {
-        return null;
+    @GetMapping("/order/{customer_id}")
+    public Order getById(@PathVariable Integer customer_id) {
+        return orderService.getById(customer_id);
     }
 
     @Override
-    public Order updateById(Integer id, Order object) {
-        return null;
+    @PutMapping("/order/{customer_id}")
+    public Order updateById(@PathVariable("customer_id") Integer id, @RequestBody Order order) {
+        return orderService.updateById(id, order);
     }
 
     @Override
-    public void removeById(Integer id) {
-
+    @PatchMapping("/order/{customer_id}")
+    public void removeById(@PathVariable Integer customer_id) {
+        orderService.removeById(customer_id);
     }
 }
--- src/main/java/com/hillel/store/controller/product/ProductControllerBean.java
+++ src/main/java/com/hillel/store/controller/product/ProductControllerBean.java
@@ -1,32 +1,49 @@
 package com.hillel.store.controller.product;
 
+import com.hillel.store.domain.Address;
 import com.hillel.store.domain.Product;
+import com.hillel.store.service.order.OrderService;
+import com.hillel.store.service.product.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
+@AllArgsConstructor
+@RequestMapping(value = "/store_api", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ProductControllerBean implements ProductController {
+
+    private final ProductService productService;
+
     @Override
-    public Product create(Product object) {
-        return null;
+    @PostMapping("/product")
+    public Product create(@RequestBody Product product) {
+        return productService.create(product);
     }
 
     @Override
+    @GetMapping("/product")
     public List<Product> getAll() {
-        return null;
+        return productService.getAll();
     }
 
     @Override
-    public Product getById(Integer id) {
-        return null;
+    @GetMapping("/product/{cart_id}")
+    public Product getById(@PathVariable Integer cart_id) {
+        return productService.getById(cart_id);
     }
 
     @Override
-    public Product updateById(Integer id, Product object) {
-        return null;
+    @PutMapping("/product/{cart_id}")
+    public Product updateById(@PathVariable("cart_id") Integer id, @RequestBody Product product) {
+        return productService.updateById(id, product);
     }
 
     @Override
-    public void removeById(Integer id) {
-
+    @PatchMapping("/product/{cart_id}")
+    public void removeById(@PathVariable Integer cart_id) {
+        productService.removeById(cart_id);
     }
 }
