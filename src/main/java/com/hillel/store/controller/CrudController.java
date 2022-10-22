a286e0d450060a36530c2fcfcdb94bc24b4786b1
Viacheslav Dubovskyi <architect1slava@gmail.com>
2022-10-22 15:50:19 +0300

    added address & customer controllers

--- src/main/java/com/hillel/store/controller/CrudController.java
+++ src/main/java/com/hillel/store/controller/CrudController.java
@@ -1,16 +1,24 @@
 package com.hillel.store.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import java.util.List;
 
 public interface CrudController<T> {
 
+    @ResponseStatus(HttpStatus.CREATED)
     T create(T object);
 
+    @ResponseStatus(HttpStatus.OK)
     List<T> getAll();
 
+    @ResponseStatus(HttpStatus.OK)
     T getById(Integer id);
 
+    @ResponseStatus(HttpStatus.OK)
     T updateById(Integer id, T object);
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     void removeById(Integer id);
 }
--- src/main/java/com/hillel/store/controller/customer/CustomerControllerBean.java
+++ src/main/java/com/hillel/store/controller/customer/CustomerControllerBean.java
@@ -1,32 +1,47 @@
 package com.hillel.store.controller.customer;
 
 import com.hillel.store.domain.Customer;
+import com.hillel.store.service.customer.CustomerService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
+@AllArgsConstructor
+@RequestMapping(value = "/store_api", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CustomerControllerBean implements CustomerController {
+
+    private final CustomerService customerService;
+
     @Override
-    public Customer create(Customer object) {
-        return null;
+    @PostMapping("/customer")
+    public Customer create(@RequestBody Customer customer) {
+        return customerService.create(customer);
     }
 
     @Override
+    @GetMapping("/customer")
     public List<Customer> getAll() {
-        return null;
+        return customerService.getAll();
     }
 
     @Override
-    public Customer getById(Integer id) {
-        return null;
+    @GetMapping("/customer/{id}")
+    public Customer getById(@PathVariable Integer id) {
+        return customerService.getById(id);
     }
 
     @Override
-    public Customer updateById(Integer id, Customer object) {
-        return null;
+    @PutMapping("/customer/{id}")
+    public Customer updateById(@PathVariable("id") Integer id, Customer customer) {
+        return customerService.updateById(id, customer);
     }
 
     @Override
-    public void removeById(Integer id) {
-
+    @PatchMapping("/customer/{id}")
+    public void removeById(@PathVariable Integer id) {
+        customerService.removeById(id);
     }
 }
