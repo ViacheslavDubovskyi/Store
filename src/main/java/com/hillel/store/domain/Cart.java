5abbfdef7abd547ea12a770d0758e6a24727401f
MaxKR1994 <98046645+MaxKR1994@users.noreply.github.com>
2022-10-24 21:49:08 +0300

    added field

--- src/main/java/com/hillel/store/domain/Cart.java
+++ src/main/java/com/hillel/store/domain/Cart.java
@@ -18,7 +18,7 @@ public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
+    private Double totalPrice;
     @ManyToMany
     @JoinTable(
             name = "cart_product",
